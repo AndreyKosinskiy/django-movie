5f6f97ab4c8c47e554be71c4e9f59c5189db820a
unknown <kosinskiy.andrey@ukr.net>
2020-03-30 18:14:20 +0300

    added forms for reviews and movieshots

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render 
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import View, ListView, DetailView
 from .models import Movie
+from .forms import ReviewForm
 # Create your views here.
 
 class MovieViews(ListView):
     """ Вывов списка фильмов """
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    template_name = 'movies/movies.html'
 
 class MovieDetailView(DetailView):
+    """ Информация про фильм """
     model = Movie
     slug_field = 'url'
+
+class AddReview(View):
+    """ Отзыв """
+    def post(self, request, pk):
+        movie = Movie.objects.get(id = pk)
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
