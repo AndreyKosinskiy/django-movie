147d0a2b712ac3e04b9efce0da937634ef338624
unknown <kosinskiy.andrey@ukr.net>
2020-04-01 15:48:19 +0300

    added show actro and directors detail.

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View, ListView, DetailView
-from .models import Movie
+from .models import Movie, Actor
 from .forms import ReviewForm
 # Create your views here.
 
@@ -26,3 +26,9 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+class ActorDetailView(DetailView):
+    """ Детали Актёра или режиссера """
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = 'name'
